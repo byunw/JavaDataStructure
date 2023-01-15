7f3d0c79ae9af90bd6523f1169df282a0a77de43
byunw <wbyun1461@gmail.com>
2023-01-15 16:36:32 +0900

    Create ArrrayTraversal.java

---
+++ FixedArray/ArrrayTraversal.java
@@ -0,0 +1,12 @@
+public class ArrayTraversal{
+  
+    public static void main(String[] args){
+            int[] numbers = {1,3,4};
+            
+            //어레이 값 순차적으로 출력             
+            for(int i=0;i<=2;++i){
+                System.out.println(numbers[i]);
+            }
+    }
+    
+}
