2a66cc35b282744d1a55c564bd9dfffb2f2b1932
byunw <wbyun1461@gmail.com>
2023-01-25 18:14:17 +0900

    Add files via upload

---
+++ LinkedList/LinkedListInsertionBegin.java
@@ -0,0 +1,24 @@
+public class LinkedListInsertionBegin {
+    public static void main(String[] args){
+
+        //2개 노드를 가진 링크리스트 생성
+        LinkedList list = new LinkedList();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+
+        //기존 첫번쨰 노드앞에 새로운 노드 삽입
+        Node temp = list.head;
+        list.head = new Node(0);    
+        list.head.next = temp;
+
+        Node currentNode = list.head;
+        while(currentNode!=null){
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    
+        System.out.println("새로 삽입된 노드의 데이터까지 다 출력!");
+        
+    }   
+    
+}
