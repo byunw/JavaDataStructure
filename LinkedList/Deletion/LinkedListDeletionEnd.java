ce20af40e9a7710e42040562a138352160818a8b
byunw <wbyun1461@gmail.com>
2023-01-26 11:56:27 +0900

    Update LinkedListDeletionEnd.java

--- LinkedList/Deletion/LinkedListDeletionEnd.java
+++ LinkedList/Deletion/LinkedListDeletionEnd.java
@@ -1,7 +1,8 @@
 public class LinkedListDeletionEnd {
+    
     public static void main(String[] args){
 
-        //노드 3개 가진 링크링스트 생성
+        //노드 3개 가진 링크스트 생성
         LinkedList ll = new LinkedList();
         ll.head = new Node(1);
         ll.head.next = new Node(2);
@@ -10,7 +11,7 @@ public class LinkedListDeletionEnd {
         //마지막 노드 삭제
         ll.head.next.next = null;
         
-        //삭제 후 존재하는 노드들을 데이터 출력
+        //삭제 후 존재하는 노드들의 데이터 출력
         Node currentNode = ll.head;
         while(currentNode!=null){
              System.out.println(currentNode.data);
